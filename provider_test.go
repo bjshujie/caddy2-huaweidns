2640e537038499fe4b7e80573910373d62e928a2
yangdacheng <george@bjshujie.com>
2023-03-30 09:14:22 +0800

    初始提交。

--- provider_test.go
+++ provider_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestProvider_GetRecords(t *testing.T) {
 	p := &Provider{
-		AccessKey:       "15ECIPCQCLRKG5TI4FDH",
-		SecretAccessKey: "XMB3UMuDXz2LKRYYfCOOqAqUI26DS3neL7YqS93f",
-		RegionID:        "cn-north-4",
+		AccessKey:       "**",
+		SecretAccessKey: "**",
+		RegionID:        "**",
 		EndPoint:        "https://dns.cn-north-4.myhuaweicloud.com/",
-		ZoneId:          "8aace3ba66f3b68501672f3659a43053",
+		ZoneId:          "**",
 	}
 	p.initClient()
 	records, err := p.GetRecords(context.Background(), "bjshujie.com")
